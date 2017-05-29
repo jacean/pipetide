fb1965f027f0507c075535c49131adccbf6b292f
jacean <jaceanwan@gmail.com>
2017-05-29 22:01:19 +0800

    fix:每次启动都会选中第一个，jslide插件添加是否默认选中的选项selected

--- src/js/jSlide.js
+++ src/js/jSlide.js
@@ -27,7 +27,7 @@
             this.bindLinkEvent();
             this.bindTabEvent();
 
-            
+
             var event = self.event;
             /**
              * 控制显示隐藏
@@ -36,22 +36,25 @@
             control.click(function () {
                 if (control.hasClass("slide-head-show")) {
                     control.removeClass('slide-head-show');
-                     control.addClass('slide-head-hide');
+                    control.addClass('slide-head-hide');
                     control.removeClass('glyphicon glyphicon-chevron-left');
                     control.addClass('glyphicon glyphicon-chevron-right');
-                    $('.slide-container').css('opacity','0');
+                    $('.slide-container').css('opacity', '0');
                     event.hide();
                 } else {
                     control.removeClass('slide-head-hide');
                     control.addClass('slide-head-show');
                     control.removeClass('glyphicon glyphicon-chevron-right');
-                     control.addClass('glyphicon glyphicon-chevron-left');
-                     event.show();
-                     $('.slide-container').css({'opacity':1});
+                    control.addClass('glyphicon glyphicon-chevron-left');
+                    event.show();
+                    $('.slide-container').css({ 'opacity': 1 });
                 }
             })
-            var $tabs = self.$main.find("[data-type='tab']");
-            if ($tabs.length > 0) $tabs[0].click();
+            if (self.options.selected) {
+                var $tabs = self.$main.find("[data-type='tab']");
+                if ($tabs.length > 0) $tabs[0].click();
+            }
+
         },
         createElement: function () {
             var self = this;
@@ -119,7 +122,7 @@
 
             function createHeader(headerObject) {
                 var headerHtml = '<div class="slide-header">';
-                
+
                 if (typeof headerObject == "string") {
                     headerHtml += '<div class="slide-header-content">' + headerObject + '</div>';
                 }
@@ -143,7 +146,7 @@
                 return footerHtml;
             }
             //add hide button 2017.5.1
-            var Html= "<div class='slide-control slide-head-show glyphicon glyphicon-chevron-left'></div>"
+            var Html = "<div class='slide-control slide-head-show glyphicon glyphicon-chevron-left'></div>"
             Html += ' <div class="slide-container">';
             if (data instanceof Array) {
                 /**是数组，表示只传入了body */
--- src/view/template/slide.ctrl.js
+++ src/view/template/slide.ctrl.js
@@ -2,6 +2,7 @@ var ctrl_slide_init = function () {
     //初始化左边栏
     jUI.jSlide("#slideContainer", {
         data: data_slide_data,
-        event: data_slide_event
+        event: data_slide_event,
+        selected:false
     });
 }
